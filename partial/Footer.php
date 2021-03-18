a747069f693601f1af45bec22286b23b3fa40bfe
Riguz Lee <solee.linux@gmail.com>
2021-03-18 19:37:56 +0800

    fix footer style

--- partial/Footer.php
+++ partial/Footer.php
@@ -1,12 +1,14 @@
 <footer class="footer">
     <div class="content has-text-centered">
-        <div class="columns">
-            <?php foreach ($this->getFooterLinks('flat') as $key) { ?>
+
+        <?php foreach ($this->getFooterLinks('flat') as $key) { ?>
+            <div class="columns">
                 <div class="column is-full">
                     <small><?php $this->html($key) ?></small>
                 </div>
-            <?php } ?>
-        </div>
+            </div>
+        <?php } ?>
+
         <div class="columns">
             <?php foreach ($this->getFooterIcons('icononly') as $blockName => $footerIcons) { ?>
                 <div class="column">
